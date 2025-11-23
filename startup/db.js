b039a57a85e5d359dbbc00a094eca61344533414
Benik <benik.karapetyan1@gmail.com>
2025-11-23 14:41:16 +0400

    changed db connection string

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = () => {
-  mongoose.connect("mongodb://localhost:27017/igrusha_pro").then(() => {
-    winston.info("Connected to MongoDB...");
-  });
+  mongoose
+    .connect(
+      "mongodb+srv://benikkarapetyan:SOHrNgfZozVTdKqP@cluster0.7tueots.mongodb.net/?appName=Cluster0"
+    )
+    .then(() => {
+      winston.info("Connected to MongoDB...");
+    });
 };
