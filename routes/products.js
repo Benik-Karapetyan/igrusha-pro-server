50ba5e4743964ae997081f451c3292a8fd130bba
Benik <benik.karapetyan1@gmail.com>
2026-01-22 22:42:56 +0400

    fix: 🐛 minor

--- routes/products.js
+++ routes/products.js
@@ -78,15 +78,17 @@ router.get("/back-office", [auth, admin], async (req, res) => {
   });
 });
 
-router.get("/:urlName", async (req, res) => {
-  const product = await Product.findOne({ urlName: req.params.urlName })
+router.get("/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).send("The product with the given ID was not found.");
+  }
+
+  let product = await Product.findById(req.params.id)
     .populate({ path: "variants", select: "-__v -cost" })
     .populate({ path: "relatedProducts", select: "-__v -cost" })
     .select("-__v -cost");
   if (!product)
-    return res
-      .status(404)
-      .send("The product with the given URL name was not found.");
+    return res.status(404).send("The product with the given ID was not found.");
 
   if (product.isVariantOf) {
     product = await Product.findById(product.isVariantOf)
