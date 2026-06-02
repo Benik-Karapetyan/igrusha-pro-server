a81637638784e52e25ed81c6e5cc0212a9fce3da
Benik <benik.karapetyan1@gmail.com>
2026-06-02 19:56:18 +0400

    fix: 🐛 minor

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -14,7 +14,6 @@ const confirmReturnOrder = require("./confirmReturnOrder");
 const deleteOrder = require("./deleteOrder");
 const getOrderPaymentStatus = require("./getOrderPaymentStatus");
 const completeOrderPayment = require("./completeOrderPayment");
-const backfillItemPrices = require("./backfillItemPrices");
 
 router.get("/", auth, getOrders);
 
@@ -25,9 +24,6 @@ router.get("/:id", auth, getOrderById);
 router.post("/", auth, createOrder);
 
 router.post("/admin", [auth, admin], createOrderAdmin);
-
-// TEMPORARY: one-off backfill of order item prices. Remove after running.
-router.post("/backfill-item-prices", [auth, admin], backfillItemPrices);
 
 router.put("/:id/admin", [auth, admin], updateOrderAdmin);
 
