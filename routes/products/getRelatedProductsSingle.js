c65946469e84f155e0eb28e04706ada1620e15ea
Benik <benik.karapetyan1@gmail.com>
2026-06-14 22:07:22 +0400

    feat: 🎸 add related by brand filtering

--- routes/products/getRelatedProductsSingle.js
+++ routes/products/getRelatedProductsSingle.js
@@ -1,5 +1,43 @@
 const { Product } = require("../../models/product");
 
+const buildFallbackQuery = (product, excludedIds, includeBrand) => {
+  const relatedProductsQuery = {
+    categories: { $in: product.categories },
+    _id: { $nin: excludedIds },
+    isVariantOf: { $ne: product._id },
+    isPublished: true,
+  };
+
+  if (includeBrand && product.brand) {
+    relatedProductsQuery.brand = product.brand;
+  }
+
+  const productAgeFrom = product.ageRange?.from;
+  const productAgeTo = product.ageRange?.to ?? Number.MAX_SAFE_INTEGER;
+
+  if (typeof productAgeFrom === "number") {
+    relatedProductsQuery.$expr = {
+      $and: [
+        { $lte: ["$ageRange.from", productAgeTo] },
+        {
+          $gte: [
+            { $ifNull: ["$ageRange.to", Number.MAX_SAFE_INTEGER] },
+            productAgeFrom,
+          ],
+        },
+      ],
+    };
+  }
+
+  return relatedProductsQuery;
+};
+
+const findFallbackRelatedProducts = (query, limit) =>
+  Product.find(query)
+    .populate({ path: "categories", select: "-__v" })
+    .select("-__v -cost")
+    .limit(limit);
+
 const getRelatedProductsSingle = async (req, res) => {
   const product = await Product.findById(req.params.id).populate({
     path: "relatedProducts",
@@ -13,44 +51,33 @@ const getRelatedProductsSingle = async (req, res) => {
   const existingRelatedProducts = product.relatedProducts.filter(
     (relatedProduct) => relatedProduct.isPublished
   );
-  const remainingCount = Math.max(10 - existingRelatedProducts.length, 0);
+  let remainingCount = Math.max(10 - existingRelatedProducts.length, 0);
 
-  let fallbackRelatedProducts = [];
+  const fallbackRelatedProducts = [];
 
   if (remainingCount > 0) {
-    const excludedIds = [
+    let excludedIds = [
       product._id,
       ...existingRelatedProducts.map((relatedProduct) => relatedProduct._id),
     ];
 
-    const relatedProductsQuery = {
-      categories: { $in: product.categories },
-      _id: { $nin: excludedIds },
-      isVariantOf: { $ne: product._id },
-      isPublished: true,
-    };
-
-    const productAgeFrom = product.ageRange?.from;
-    const productAgeTo = product.ageRange?.to ?? Number.MAX_SAFE_INTEGER;
-
-    if (typeof productAgeFrom === "number") {
-      relatedProductsQuery.$expr = {
-        $and: [
-          { $lte: ["$ageRange.from", productAgeTo] },
-          {
-            $gte: [
-              { $ifNull: ["$ageRange.to", Number.MAX_SAFE_INTEGER] },
-              productAgeFrom,
-            ],
-          },
-        ],
-      };
+    if (product.brand) {
+      const byBrandAndCategory = await findFallbackRelatedProducts(
+        buildFallbackQuery(product, excludedIds, true),
+        remainingCount
+      );
+      fallbackRelatedProducts.push(...byBrandAndCategory);
+      excludedIds.push(...byBrandAndCategory.map((p) => p._id));
+      remainingCount -= byBrandAndCategory.length;
     }
 
-    fallbackRelatedProducts = await Product.find(relatedProductsQuery)
-      .populate({ path: "categories", select: "-__v" })
-      .select("-__v -cost")
-      .limit(remainingCount);
+    if (remainingCount > 0) {
+      const byCategory = await findFallbackRelatedProducts(
+        buildFallbackQuery(product, excludedIds, false),
+        remainingCount
+      );
+      fallbackRelatedProducts.push(...byCategory);
+    }
   }
 
   res.send([...existingRelatedProducts, ...fallbackRelatedProducts]);
